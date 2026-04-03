703ce9bc4d36d3da8b99d6c0f3704be725f240d6
Anil-kumar-Akula <anilroyal8999@gmail.com>
2026-04-03 19:27:06 +0530

    Refactor workstation.sh for improved installations
    Updated installation commands for Docker, kubectl, and eksctl in workstation.sh. Improved script structure and added version checks.

--- workstation.sh
+++ workstation.sh
@@ -4,22 +4,42 @@ lvextend -L +30G /dev/mapper/RootVG-varVol
 xfs_growfs /var
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-systemctl start docker
+
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 systemctl enable docker
+systemctl start docker
+
 usermod -aG docker ec2-user
+KUBECTL_VERSION="v1.34.2"
 
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-mkdir -p $HOME/bin && cp ./kubectl  /usr/local/bin && export PATH=$HOME/bin:$PATH
-
+curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mv kubectl /usr/local/bin/
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
-curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_${PLATFORM}.tar.gz"
+tar -xzf eksctl_${PLATFORM}.tar.gz -C /tmp
+rm -f eksctl_${PLATFORM}.tar.gz
+
+install -m 0755 /tmp/eksctl /usr/local/bin/eksctl
+rm -f /tmp/eksctl
+echo 'export PATH=/usr/local/bin:$PATH' >> /etc/profile.d/custom-path.sh
+chmod +x /etc/profile.d/custom-path.sh
+source /etc/profile.d/custom-path.sh
+
+hash -r
+echo "Docker Version:"
+docker --version
+
+echo "Kubectl Version:"
+kubectl version --client
+
+echo "eksctl Version:"
+eksctl version
 mkdir -p /opt/eks
+
 
 cat >/opt/eks/cluster.yaml <<'EOC'
 apiVersion: eksctl.io/v1alpha5
